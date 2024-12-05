c9cc3272c3bf634dfe31fbee61ceae846eb39afe
OatyQuinoa <renewright20@gmail.com>
2024-12-05 17:57:11 +0800

    Initiated testing for assortment of characters according to number of rows (rails)

--- Rail Fence/railfence.js
+++ Rail Fence/railfence.js
@@ -15,3 +15,32 @@ rl.question("Enter a message: ", (message) => {
 function rail_fence(message, num_rails) {
     
 }
+
+function splitString(input, rail) { // Where rail = # of rows 
+    let output = [];
+    let i = 0;
+    let uprows = rail - 2
+
+    while (i < input.length) {
+        if (i + rail <= input.length) { // 1
+            output.push(input.substring(i, i + rail))
+            console.log(i, output)
+            i += 3 // Increment by 3 to substring first 3 letters 
+        }
+
+        if (i < input.length) { // 2
+            output.push(input.substring(i, i + uprows))
+            console.log(i, output)
+            i += uprows // Increment by # of rails to substring # rails of letters 
+        }
+
+        // cycle repeats from 1-2-1-2 . . . 
+    }
+    
+    console.log("Testing: ", output[0][2])
+
+    return output
+}
+
+//console.log(splitString("CARECARECAR", 3))
+console.log(splitString("APPLEBEE", 3))
