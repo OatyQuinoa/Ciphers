397354bd0885d8ca00752dca50db75525091356e
OatyQuinoa <renewright20@gmail.com>
2024-12-16 18:38:33 +0800

    Coded standard form of bitwise stream cipher using XOR operation. So far a bit key is problematic because ASCII chars 0 - 32 and 127 are nonprintable. Hexadecimal keys opted.

--- Bit/bit.js
+++ Bit/bit.js
@@ -12,13 +12,45 @@ rl.question("Enter a message to encode", (message) => {
 */
 
 function bitwise_cipher(message) {
+    // Convert message to bit form: 
     let binary_message = message
         .split("")
         .map(char => char.charCodeAt(0).toString(2).padStart(8, "0"))
-        .join(" ")
-    return binary_message
+        .join("")
+
+    // Create a binary key to XOR each bit from binary_message: 
+    let binary_key = Array.from({length: binary_message.length}, () => Math.random() < 0.5 ? '0' : '1').join("")
+    // Use temporary binary key: 0001100010110001
+
+    let temp_key = "0001100010110001"
+    let bitwise_XOR_output = ""
+    for (i = 0; i < binary_message.length; i++) {
+        bitwise_XOR_output += binary_message[i] ^ temp_key[i] // ^ denotes XOR, where equal bits returns 0 and nonequal bits returns 1
+    }
+
+    let decimal_arr = []
+    let char_arr = []
+    let char_output = ""
+
+    let left_counter = 0 
+    let right_counter = 8
+
+    while (right_counter <= bitwise_XOR_output.length) {
+        let substr = bitwise_XOR_output.substring(left_counter, right_counter)
+        decimal_arr.push(parseInt(substr, 2))
+
+        left_counter += 8
+        right_counter += 8
+    }
+
+    for (i = 0; i < decimal_arr.length; i++) {
+        let char = String.fromCharCode(decimal_arr[i])
+        char_arr.push(char)
+        char_output += char
+    }
+
+    return [bitwise_XOR_output, decimal_arr, char_arr, char_output]
 }
 
-console.log(bitwise_cipher("ABC"))
-console.log("Exclusive OR (XOR), if given 1 and 0", 1 ^ 0)
-console.log("Exclusive OR (XOR), if given 1 and 1", 1 ^ 1)
+
+console.log("Ans: ", bitwise_cipher("44"))
